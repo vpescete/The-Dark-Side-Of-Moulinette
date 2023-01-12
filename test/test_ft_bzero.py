644e33b3db33d83e0b47efdf74ed6d924d9ffc5c
Valerio Pescetelli <valeriopescetellinl@gmail.com>
2023-01-12 16:16:45 +0100

    Fixed test_ft_bzero.py

--- test/test_ft_bzero.py
+++ test/test_ft_bzero.py
@@ -1,6 +1,6 @@
 import ctypes
-
 import os
+import pytest
 
 # Get the current working directory
 cwd = os.getcwd()
@@ -11,67 +11,25 @@ library_path = os.path.join(cwd, 'libft.so')
 libft = ctypes.cdll.LoadLibrary(library_path)
 libc = ctypes.cdll.LoadLibrary('libc.so.6')
 
-def test1_ft_bzero():
-  # Test input di esempio
-  input = [0, 1, 2, 3, 4]
-  
-  # Definisci le funzioni ft_bzero e bzero
-  ft_bzero = libft.ft_bzero
-  bzero = libc.bzero
+input_lists = [bytearray([0, 1, 2, 3, 4]), bytearray([0]), bytearray([0, 0, 0 ,0 ,0]), bytearray([0, 1]), bytearray([])]
+ids = ["input: '{}'".format(t) for t in input_lists]
+@pytest.mark.parametrize("input_list", input_lists, ids = ids)
 
-  input_ptr = ctypes.c_void_p(id(input))
+def test_ft_bzero(input_list):
+    # Test input
+    input_l = input_list
+    input_buf = ctypes.create_string_buffer(bytes(input_l))
+    input_ptr = ctypes.cast(input_buf, ctypes.POINTER(ctypes.c_byte))
+    input_len = len(input_l)
+    
+    # Define the ft_bzero function
+    ft_bzero = libft.ft_bzero
+    bzero = libc.bzero
 
-  # Verifica che l'output delle funzioni sia corretto
-  assert ft_bzero(input_ptr, len(input)) == bzero(input_ptr, len(input))
+    ft_bzero(input_ptr, input_len)
+    bzero(input_ptr, input_len)
 
-def test2_ft_bzero():
-  # Test input di esempio
-  input = [0]
-  
-  # Definisci le funzioni ft_bzero e bzero
-  ft_bzero = libft.ft_bzero
-  bzero = libc.bzero
-
-  input_ptr = ctypes.c_void_p(id(input))
-
-  # Verifica che l'output delle funzioni sia corretto
-  assert ft_bzero(input_ptr, len(input)) == bzero(input_ptr, len(input))
-
-def test3_ft_bzero():
-  # Test input di esempio
-  input = [1, 1, 4, 3, 4, 1, 2, 4]
-  
-  # Definisci le funzioni ft_bzero e bzero
-  ft_bzero = libft.ft_bzero
-  bzero = libc.bzero
-
-  input_ptr = ctypes.c_void_p(id(input))
-
-  # Verifica che l'output delle funzioni sia corretto
-  assert ft_bzero(input_ptr, len(input)) == bzero(input_ptr, len(input))
-
-def test4_ft_bzero():
-  # Test input di esempio
-  input = []
-  
-  # Definisci le funzioni ft_bzero e bzero
-  ft_bzero = libft.ft_bzero
-  bzero = libc.bzero
-
-  input_ptr = ctypes.c_void_p(id(input))
-
-  # Verifica che l'output delle funzioni sia corretto
-  assert ft_bzero(input_ptr, len(input)) == bzero(input_ptr, len(input))
-
-def test5_ft_bzero():
-  # Test input di esempio
-  input = [0, 1, 2, 3, 4, 'c']
-  
-  # Definisci le funzioni ft_bzero e bzero
-  ft_bzero = libft.ft_bzero
-  bzero = libc.bzero
-
-  input_ptr = ctypes.c_void_p(id(input))
-
-  # Verifica che l'output delle funzioni sia corretto
-  assert ft_bzero(input_ptr, len(input)) == bzero(input_ptr, len(input))
+    # Check that all bytes in the input_buf are set to 0
+    input_buf_str = ctypes.string_at(input_ptr, input_len)
+    assert input_buf_str.count(0) == input_len
+    assert all(i == 0 for i in input_buf_str)
